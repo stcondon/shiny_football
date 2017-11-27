79f38f68942db3118cdc317cfd5ec4af6710612a
Stephen Condon <stcondon@gmail.com>
2017-11-26 21:58:52 -0800

    built points

--- functions/tb.R
+++ functions/tb.R
@@ -1,4 +1,8 @@
 library(data.table)
 tb <- function(dt, country = 'England') {
-  teams <- unique(england[[1]]$HomeTeam)
+  rbindlist(list(dt[FTR == 'H',.(p = uniqueN(AwayTeam) * 3), by = 'HomeTeam'],
+                 dt[FTR == 'A',.(p = uniqueN(HomeTeam) * 3), by = 'AwayTeam'],
+                 dt[FTR == 'D',.(p = uniqueN(AwayTeam)), by = 'HomeTeam'],
+                 dt[FTR == 'D',.(p = uniqueN(HomeTeam)), by = 'AwayTeam'])
+            )[,lapply(.SD,sum,na.rm=TRUE),by=HomeTeam][order(p,decreasing=TRUE)]
 }

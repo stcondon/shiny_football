b86e71bbb93ef5e901f3a199520c1969619d9077
Stephen Condon <stcondon@gmail.com>
2017-11-27 21:57:11 -0800

    built english table

--- functions/tb.R
+++ functions/tb.R
@@ -1,6 +1,7 @@
 library(data.table)
 tb <- function(dt, country = 'England') {
   ## Cut down table to just full time goals and results
+  # dt <- dt[,c("HomeTeam","AwayTeam","FTR","FTHG","FTAG")]
   t <- rbindlist(list(dt[FTR == 'H', .(p = uniqueN(AwayTeam) * 3),
                          by = 'HomeTeam'],
                       dt[FTR == 'A', .(p = uniqueN(HomeTeam) * 3),
@@ -10,11 +11,17 @@ tb <- function(dt, country = 'England') {
                       dt[FTR == 'D', .(p = uniqueN(HomeTeam)),
                          by = 'AwayTeam']))[,lapply(.SD,sum,na.rm=TRUE),
                                             by=HomeTeam]
-  if(sum(duplicated(t$p)) > 1) {
-    if(country %in% c('England')) {} else if(country %in% c('Spain')) {
-      #t <-
-    }
+  t <- merge(t,rbindlist(list(dt[, .(scored = sum(FTHG)), by = 'HomeTeam'],
+                              dt[,sum(FTAG), by = 'AwayTeam']))
+             [,lapply(.SD, sum, na.rm = TRUE), by=HomeTeam], by = 'HomeTeam',
+             all = TRUE)
+  t <- merge(t,rbindlist(list(dt[, .(conceded = sum(FTAG)), by = 'HomeTeam'],
+                              dt[,sum(FTHG), by = 'AwayTeam']))
+             [,lapply(.SD, sum, na.rm = TRUE), by=HomeTeam], by = 'HomeTeam',
+             all = TRUE)
+  if(country %in% c('Spain')) {
+      ## h2h function}
   } else {
-    t[order(p,decreasing = TRUE)]
+    t[order(p,scored - conceded, scored, decreasing = TRUE)]
   }
 }

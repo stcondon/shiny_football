f7ad40e0861a479198c82c903633009deb3ea674
Stephen Condon <stcondon@gmail.com>
2017-12-28 14:59:35 -0600

    create tb off whatever country is input

--- functions/totteringham.R
+++ functions/totteringham.R
@@ -2,4 +2,5 @@ library(data.table)
 totteringham <- function(dt, team_1 = 'Arsenal', team_2 = 'Tottenham',
                          country = 'England') {
   t <- dt[HomeTeam %in% c(team_1, team_2) | AwayTeam %in% c(team_1, team_2)]
+  tb <- match.fun(paste0(tolower(country), '_tb'))
 }

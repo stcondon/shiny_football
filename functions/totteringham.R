679db26b76c6b7b7f12f6f0ad94a7274694e64fc
Stephen Condon <stcondon@gmail.com>
2017-12-28 15:41:52 -0600

    add a team count, limit dt to only relevant games, turn t into table of just relevant teams, create games left variables

--- functions/totteringham.R
+++ functions/totteringham.R
@@ -1,6 +1,11 @@
 library(data.table)
 totteringham <- function(dt, team_1 = 'Arsenal', team_2 = 'Tottenham',
                          country = 'England') {
-  t <- dt[HomeTeam %in% c(team_1, team_2) | AwayTeam %in% c(team_1, team_2)]
+  tc <- length(unique(c(dt$HomeTeam, dt$AwayTeam)))
+  dt <- dt[HomeTeam %in% c(team_1, team_2) | AwayTeam %in% c(team_1, team_2)]
   tb <- match.fun(paste0(tolower(country), '_tb'))
+  t <- tb(dt)[HomeTeam %in% c(team_1, team_2)]
+  t1_gl <- 2 * (tc - 1) - nrow(dt[HomeTeam == team_1 | AwayTeam == team_1])
+  t2_gl <- 2 * (tc - 1) - nrow(dt[HomeTeam == team_2 | AwayTeam == team_2])
+  # if()
 }

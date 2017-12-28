4103be8c4e5a960ab21da44a35e7209639e27635
Stephen Condon <stcondon@gmail.com>
2017-12-27 18:21:29 -0600

    add a country parameter

--- functions/totteringham.R
+++ functions/totteringham.R
@@ -1,4 +1,5 @@
 library(data.table)
-totteringham <- function(dt, team_1 = 'Arsenal', team_2 = 'Tottenham') {
+totteringham <- function(dt, team_1 = 'Arsenal', team_2 = 'Tottenham',
+                         country = 'England') {
   t <- dt[HomeTeam %in% c(team_1, team_2) | AwayTeam %in% c(team_1, team_2)]
 }

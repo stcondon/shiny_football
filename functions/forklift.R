c92ba7ec5a5fa1e5852ee37ddfe3914aab1667f9
Stephen Condon <stcondon@gmail.com>
2017-12-29 12:33:43 -0600

    switched over to paste0

--- functions/forklift.R
+++ functions/forklift.R
@@ -8,22 +8,22 @@ fld <- substr(first_year, 4,4)
 lld <- substr(last_year, 4,4)
 if(as.numeric(first_year) < 2000 && as.numeric(last_year) >= 2010) {
   # /^(199\d|200\d|2010)$/
-  re <- paste('(199[', fld, '-9]|200[0-9]|201[0-', lld,
-              '])_[[:digit:]]{4}.csv$', sep = '')
+  re <- paste0('(199[', fld, '-9]|200[0-9]|201[0-', lld,
+              '])_[[:digit:]]{4}.csv$')
 } else if(as.numeric(first_year) < 2000 && as.numeric(last_year) >= 2000 &&
           as.numeric(last_year) < 2010) {
-  re <- paste('(199[', fld, '-9]|200[0-', lld,
-              '])_[[:digit:]]{4}.csv$', sep = '')
+  re <- paste0('(199[', fld, '-9]|200[0-', lld,
+              '])_[[:digit:]]{4}.csv$')
 } else if(as.numeric(first_year) < 2010 && as.numeric(first_year) >= 2000 &&
           as.numeric(last_year) >= 2010) {
-  re <- paste('(200[', fld, '-]|201[0-', lld,
-              '])_[[:digit:]]{4}.csv$', sep = '')
+  re <- paste0('(200[', fld, '-]|201[0-', lld,
+              '])_[[:digit:]]{4}.csv$')
 } else {
-  re <- paste('(', substr(first_year,1,3), '[', fld, '-', lld,
-              '])_[[:digit:]]{4}.csv$', sep = '')
+  re <- paste0('(', substr(first_year,1,3), '[', fld, '-', lld,
+              '])_[[:digit:]]{4}.csv$')
 }
 patron <- paste(country, tier, re, sep = '_')
-temp <- list.files(path = paste('data/',tolower(country),'/', sep = ''),
+temp <- list.files(path = paste0('data/',tolower(country),'/'),
                    pattern= patron)
-lapply(paste('data/',tolower(country),'/', temp, sep = ''), fread)
+lapply(paste0('data/',tolower(country),'/', temp), fread)
 }

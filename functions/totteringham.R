c46a845361418d94b6b08c1b2d4e50776220d85f
Stephen Condon <stcondon@gmail.com>
2017-12-30 19:40:35 -0600

    successful test!

--- functions/totteringham.R
+++ functions/totteringham.R
@@ -9,6 +9,18 @@ totteringham <- function(dt, team_1 = 'Arsenal', team_2 = 'Tottenham',
   t2_gl <- 2 * (tc - 1) - nrow(dt[HomeTeam == team_2 | AwayTeam == team_2])
   if(t[HomeTeam == team_2,p] > t1_gl * 3 + t[HomeTeam == team_1,p]) {
     result <- "Sorry, blood, no St Totteringham's Day this year"
+  } else if(t[HomeTeam == team_1,p] > t2_gl * 3 + t[HomeTeam == team_2,p]) {
+    dates <- dt$Date
+    for(gameday in c(1:length(dates))) {
+      dt <- head(dt, -1)
+      t <- tb(dt)[HomeTeam %in% c(team_1, team_2)]
+      t1_gl <- 2 * (tc - 1) - nrow(dt[HomeTeam == team_1 | AwayTeam == team_1])
+      t2_gl <- 2 * (tc - 1) - nrow(dt[HomeTeam == team_2 | AwayTeam == team_2])
+      if(t[HomeTeam == team_1,p] <= t2_gl * 3 + t[HomeTeam == team_2,p]) {
+        result <- paste('Judgement Day: ', dates[nrow(dt) + 1])
+        break
+      }
+    }
   }
-  # else if()
+  result
 }

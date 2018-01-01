fffb8320ca11071679087029b78eac1ade29b9cb
Stephen Condon <stcondon@gmail.com>
2017-12-31 18:01:44 -0600

    not always totteringham

--- functions/totteringham.R
+++ functions/totteringham.R
@@ -8,7 +8,7 @@ totteringham <- function(dt, team_1 = 'Arsenal', team_2 = 'Tottenham',
   t1_gl <- 2 * (tc - 1) - nrow(dt[HomeTeam == team_1 | AwayTeam == team_1])
   t2_gl <- 2 * (tc - 1) - nrow(dt[HomeTeam == team_2 | AwayTeam == team_2])
   if(t[HomeTeam == team_2,p] > t1_gl * 3 + t[HomeTeam == team_1,p]) {
-    result <- "Sorry, blood, no St Totteringham's Day this year"
+    result <- "Sorry, blood, not this year"
   } else if(t[HomeTeam == team_1,p] > t2_gl * 3 + t[HomeTeam == team_2,p]) {
     dates <- dt$Date
     for(gameday in c(1:length(dates))) {

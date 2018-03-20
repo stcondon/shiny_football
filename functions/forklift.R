85e420897fa7301ab5dcdb03b0f592e82be4c762
Stephen Condon <stcondon@gmail.com>
2018-03-19 21:18:52 -0700

    stupid whitespace

--- functions/forklift.R
+++ functions/forklift.R
@@ -2,28 +2,28 @@
 library(data.table)
 forklift <- function(country = 'England', tier = '1',
                    first_year = '1993', last_year = '1993', scraped = FALSE) {
-fld <- substr(first_year, 4,4)
-lld <- substr(last_year, 4,4)
-if(as.numeric(first_year) < 2000 && as.numeric(last_year) >= 2010) {
-  # /^(199\d|200\d|2010)$/
-  re <- paste0('(199[', fld, '-9]|200[0-9]|201[0-', lld,
-              '])_[[:digit:]]{4}.csv$')
-} else if(as.numeric(first_year) < 2000 && as.numeric(last_year) >= 2000 &&
-          as.numeric(last_year) < 2010) {
-  re <- paste0('(199[', fld, '-9]|200[0-', lld,
-              '])_[[:digit:]]{4}.csv$')
-} else if(as.numeric(first_year) < 2010 && as.numeric(first_year) >= 2000 &&
-          as.numeric(last_year) >= 2010) {
-  re <- paste0('(200[', fld, '-]|201[0-', lld,
-              '])_[[:digit:]]{4}.csv$')
-} else {
-  re <- paste0('(', substr(first_year,1,3), '[', fld, '-', lld,
-              '])_[[:digit:]]{4}.csv$')
-}
-patron <- paste(country, tier, re, sep = '_')
-## HERE WE CREATE path VARIABLE
-temp <- list.files(path = paste0('data/',tolower(country),'/'),
-                   pattern= patron)
-temp <- lapply(paste0('data/',tolower(country),'/', temp), fread)
-lapply(temp, function(x) x[!(is.na(x$FTHG))]) ## change this for scraped
+  fld <- substr(first_year, 4,4)
+  lld <- substr(last_year, 4,4)
+  if(as.numeric(first_year) < 2000 && as.numeric(last_year) >= 2010) {
+    # /^(199\d|200\d|2010)$/
+    re <- paste0('(199[', fld, '-9]|200[0-9]|201[0-', lld,
+                '])_[[:digit:]]{4}.csv$')
+  } else if(as.numeric(first_year) < 2000 && as.numeric(last_year) >= 2000 &&
+            as.numeric(last_year) < 2010) {
+    re <- paste0('(199[', fld, '-9]|200[0-', lld,
+                '])_[[:digit:]]{4}.csv$')
+  } else if(as.numeric(first_year) < 2010 && as.numeric(first_year) >= 2000 &&
+            as.numeric(last_year) >= 2010) {
+    re <- paste0('(200[', fld, '-]|201[0-', lld,
+                '])_[[:digit:]]{4}.csv$')
+  } else {
+    re <- paste0('(', substr(first_year,1,3), '[', fld, '-', lld,
+                '])_[[:digit:]]{4}.csv$')
+  }
+  patron <- paste(country, tier, re, sep = '_')
+  ## HERE WE CREATE path VARIABLE
+  temp <- list.files(path = paste0('data/',tolower(country),'/'),
+                     pattern= patron)
+  temp <- lapply(paste0('data/',tolower(country),'/', temp), fread)
+  lapply(temp, function(x) x[!(is.na(x$FTHG))]) ## change this for scraped
 }

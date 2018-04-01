224b7ccdb8882f40848ab8647d477f427e9baeb8
Stephen Condon <stcondon@gmail.com>
2018-03-31 18:18:35 -0700

    add country so you can build the durn table

--- functions/scraped_vs_pulled.R
+++ functions/scraped_vs_pulled.R
@@ -1,7 +1,8 @@
 library(data.table)
-scraped_vs_pulled <- function(scraped_dt, pulled_dt) {
+scraped_vs_pulled <- function(scraped_dt, pulled_dt, country = 'England') {
   scraped_dt <- scraped_dt[,c(1,9,6,7)]
   names(scraped_dt) <- c('HomeTeam','p', 'scored', 'conceded')
   scraped_dt$p <- as.double(scraped_dt$p)
-  identical(scraped_dt, pulled_dt)
+  tb <- match.fun(paste0(tolower(country), '_tb'))
+  identical(scraped_dt, tb(pulled_dt))
 }

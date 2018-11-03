6ba56c8caa119d29ee92535027c29c2da8c1ed4e
Stephen Condon <stcondon@gmail.com>
2018-11-03 00:23:18 -0700

    changed the forklift function to match the new directory structure

--- functions/forklift.R
+++ functions/forklift.R
@@ -1,7 +1,8 @@
 ## Build something that takes inputs and converts to list of data.tables
 library(data.table)
 forklift <- function(country = 'England', tier = '1', first_year = '1993',
-                     last_year = '1993', source = '12xpert'){
+                     last_year = '1993', source = '12xpert_matches',
+                     processed = TRUE){
   fld <- substr(first_year, 4,4)
   lld <- substr(last_year, 4,4)
   if(as.numeric(first_year) < 2000 && as.numeric(last_year) >= 2010) {
@@ -22,15 +23,16 @@ forklift <- function(country = 'England', tier = '1', first_year = '1993',
   }
   patron <- paste(country, tier, re, sep = '_')
   ## HERE WE CREATE path VARIABLE
-  if(source == 'espn') {
-    camino <- paste0('scrapy_qa/data/',tolower(country),'/')
+  if(processed == TRUE) {
+    camino <- paste0('data/', source, '/processed/', tolower(country),'/')
   } else {
-    camino <- paste0('processed_data/',tolower(country),'/')
+    camino <- paste0('data/', source, '/raw/', tolower(country),'/')
   }
+
   temp <- list.files(path = camino,
                      pattern= patron)
   temp <- lapply(paste0(camino, temp), fread)#, fill = TRUE)
-  if(source == 'espn') {
+  if(source == 'espn_tables') {
     lapply(temp, function(x) x[!(is.na(x$GP))])
   } else{
     lapply(temp, function(x) x[!(is.na(x$FTHG))])

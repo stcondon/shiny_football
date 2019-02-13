a39ddd4e68355d591034b5f9f38cac226312c29d
Stephen Condon <stcondon@gmail.com>
2019-02-13 15:47:50 -0800

    start up again, pull team names file and merge on dates

--- functions/unir.R
+++ functions/unir.R
@@ -1,2 +1,11 @@
 library(data.table)
-unir <- function(xpert, fte) {}
+unir <- function(xpert, fte, country = 'England') {
+  team_names <- fread(paste0('data/team_names/', tolower(country),'_names.csv'))
+  team_names <- merge(team_names, fte, by.x = 'fte', by.y = 'team1',
+                      all.x = TRUE, all.y = FALSE)
+  xpert$Date <- as.Date.character(xpert$Date, '%d/%m/%y')
+  # team_names$date <- as.Date(team_names$date, '%Y-%m-%d')
+  xpert <- merge(xpert, team_names, by.x = c('HomeTeam','Date'),
+                 by.y = c('xpert', 'date'), all.x = TRUE, all.y = FALSE)
+}
+

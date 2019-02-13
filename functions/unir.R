dbfb0d1ccb8b199b95fa8c1b320ae2ca6ffe6b4b
Stephen Condon <stcondon@gmail.com>
2019-02-13 15:50:38 -0800

    no need to cast, already in right format

--- functions/unir.R
+++ functions/unir.R
@@ -4,7 +4,6 @@ unir <- function(xpert, fte, country = 'England') {
   team_names <- merge(team_names, fte, by.x = 'fte', by.y = 'team1',
                       all.x = TRUE, all.y = FALSE)
   xpert$Date <- as.Date.character(xpert$Date, '%d/%m/%y')
-  # team_names$date <- as.Date(team_names$date, '%Y-%m-%d')
   xpert <- merge(xpert, team_names, by.x = c('HomeTeam','Date'),
                  by.y = c('xpert', 'date'), all.x = TRUE, all.y = FALSE)
 }
